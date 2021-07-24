6062c90fa07223294e9498de95ffea64160ed104
Thiha <thihaminzaw21@gmail.com>
2021-07-24 12:36:20 +0630

    Add remove and add function

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -13,6 +13,30 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = { searchResults: [{ name: "MiddleChild", artist: "J Cole", album: "New Era", id: 1 }, { name: "We Paid", artist: "42 Dugg, Lil Baby", album: "Smoke", id: 2 }], playlistName: "Boogey", playlistTracks: [{ name: "Bestfriends", artist: "Doja Cat, Saweetie", album: "Cow", id: 4 }] }
+    this.addTrack = this.addTrack.bind(this);
+    this.removeTrack = this.removeTrack.bind(this)
+  }
+
+  addTrack(track){
+    let tracks = this.state.playlistTracks;
+    if(tracks.find(savedTrack=>savedTrack.id===track.id)){
+      return;
+    }
+    else{
+      tracks.push(track)
+      this.setState({playlistTracks:tracks})
+    }
+
+  }
+
+  removeTrack(track){
+    let tracks = this.state.playlistTracks;
+    let index= tracks.findIndex(savedTrack=>savedTrack.id===track.id)
+    tracks.splice(index,1);
+
+    this.setState({playlistTracks:tracks})
+
+
   }
 
   render() {
@@ -25,8 +49,8 @@ class App extends React.Component {
           <SearchBar />
 
           <div className="App-playlist">
-            <SearchResults searchResults={this.state.searchResults} />
-            <Playlist playlistName={this.state.playlistName} playlistTracks={this.state.playlistTracks} />
+            <SearchResults searchResults={this.state.searchResults} onAdd={this.addTrack} />
+            <Playlist playlistName={this.state.playlistName} playlistTracks={this.state.playlistTracks} onRemove={this.removeTrack} />
 
 
 
--- src/components/Tracklist/Tracklist.js
+++ src/components/Tracklist/Tracklist.js
@@ -9,9 +9,9 @@ import Track from '../Track/Track'
 class TrackList extends React.Component{
     
     render(){
-        return(<div class="TrackList">
+        return(<div className="TrackList">
 
-            {this.props.tracks.map(track=>(<Track track={track} key={track.id} />))}
+            {this.props.tracks.map(track=>(<Track track={track} key={track.id} onAdd={this.props.onAdd} onRemove={this.props.onRemove} isRemoval={this.props.isRemoval} />))}
         
     </div>)
     }

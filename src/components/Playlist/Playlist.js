e4e3d841f14fe9bdea1004e2a6b62255c98cc143
Thiha <thihaminzaw21@gmail.com>
2021-07-24 13:09:19 +0630

    add search function

--- src/components/Playlist/Playlist.js
+++ src/components/Playlist/Playlist.js
@@ -18,7 +18,7 @@ export class Playlist extends React.Component{
 
     this.props.onNameChange(e.target.value)
 
-    
+
 
   }
     render(){
@@ -26,7 +26,7 @@ export class Playlist extends React.Component{
         <input defaultValue={'New Playlist'} onChange={e=>this.handleNameChange(e)} />
         <TrackList tracks={this.props.playlistTracks} onRemove={this.props.onRemove} isRemoval={true} />
       
-        <button className="Playlist-save">SAVE TO SPOTIFY</button>
+        <button className="Playlist-save" onClick={this.props.onSave} >SAVE TO SPOTIFY</button>
       </div>)
     }
 }
--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -4,9 +4,17 @@ import React from 'react';
 import './SearchBar.css';
 
 class SearchBar extends React.Component{
+
+  constructor(props){
+    super(props);
+    this.handleTermChange=this.handleTermChange.bind(this)
+  }
+  handleTermChange(e){
+    this.props.onSearch(e.target.value);
+  }
     render(){
         return(<div className="SearchBar">
-        <input placeholder="Enter A Song, Album, or Artist" />
+        <input placeholder="Enter A Song, Album, or Artist" onChange={e=>this.handleTermChange(e)} />
         <button className="SearchButton">SEARCH</button>
       </div>);
     }
